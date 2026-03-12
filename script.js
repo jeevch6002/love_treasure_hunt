b0e134a9ddfc039480dcedca13cad23fefadac5b
jeevch6002 <jeevch6002@gmail.com>
2026-03-12 23:45:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function createHeart(){
+
+let heart=document.createElement("div");
+
+heart.classList.add("heart");
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+heart.style.fontSize=Math.random()*20+10+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},6000);
+
+}
+
+setInterval(createHeart,300);
+
+function checkPassword(){
+
+let p=document.getElementById("pass").value.toLowerCase();
+
+if(p=="jeeva"){
+window.location.href="clue1.html";
+}
+else{
+alert("Wrong password my love ❤️");
+}
+
+}
